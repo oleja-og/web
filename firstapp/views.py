01ae6f62d8ac70b02d02b61b20fd213dbc1eb8de
oleg <oleja.og@gmail.com>
2022-03-30 15:23:43 +0300

    inital: add urls about,contact

--- firstapp/views.py
+++ firstapp/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse(" Hello")
 
+def about(request):
+    return HttpResponse("about")
+
+def contact(request):
+    return HttpResponse("contact")
+
 # Create your views here.
--- web/urls.py
+++ web/urls.py
@@ -19,5 +19,7 @@ from firstapp import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about', views.about),
+    path('contact', views.contact),
     path('admin/', admin.site.urls),
 ]
