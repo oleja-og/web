ade074550725bc32152162cfc2ca50559c9a0310
oleja-og <oleja.og@gmail.com>
2022-04-08 12:20:04 +0300

    start models

--- firstapp/models.py
+++ firstapp/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=30)
     age = models.IntegerField()
