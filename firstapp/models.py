3c9ae3edbde367395c8751fc55515cee70756761
oleja-og <oleja.og@gmail.com>
2022-04-12 16:31:34 +0300

    models onetoone

--- firstapp/models.py
+++ firstapp/models.py
@@ -23,3 +23,13 @@ class Student(models.Model):
     name = models.CharField(max_length=30)
     courses = models.ManyToManyField(Course)
 
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Account(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                primary_key= True)
