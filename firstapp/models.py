a39b0fdb0013194e32d928f7703924213e5526e4
oleja-og <oleja.og@gmail.com>
2022-04-12 12:16:42 +0300

    models manytomany

--- firstapp/models.py
+++ firstapp/models.py
@@ -5,3 +5,21 @@ class Person(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField()
     objects = models.Manager()
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Product(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course)
+
